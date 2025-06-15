7106554c2ab4a4aec0f0dc2e4c69332663c7277b
Adrian PK <adrianpk@adrianpk.com>
2025-06-15 22:54:13 +0200

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,13 @@
 
 set -e
 
-TEMPLATE_REPO="git@github.com:aquamarinepk/onset.git"
+if [ -z "$1" ]; then
+  echo "Usage: $0 <new-module-path>"
+  exit 1
+fi
 
-read -p "New module path (e.g. github.com/username/newproject): " NEW_MODULE
+NEW_MODULE="$1"
+TEMPLATE_REPO="git@github.com:aquamarinepk/onset.git"
 
 WORKDIR=$(mktemp -d)
 echo "Cloning template..."
@@ -15,9 +19,7 @@ ORIGINAL_MODULE=$(grep "^module " go.mod | awk '{print $2}')
 echo "Updating module from $ORIGINAL_MODULE to $NEW_MODULE"
 
 go mod edit -module "$NEW_MODULE"
-
 find . -type f -name "*.go" -exec sed -i "s|$ORIGINAL_MODULE|$NEW_MODULE|g" {} +
-
 rm -rf .git
 
 TARGET_DIR="../$(basename "$NEW_MODULE")"
